6e6628be77e8b2b44901101ce89665ee15777035
Lakhbir S <lakhssingh@yahoo.co.uk>
2025-11-06 08:26:25 +0000

    callback url added

--- lib/validators.ts
+++ lib/validators.ts
@@ -19,3 +19,8 @@ export const insertProductSchema = z.object({
   banner: z.string().nullable(),
   price: currency,
 });
+
+export const signInFormSchema = z.object({
+  email: z.email("Ínvalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
